24cf0e68c29fa6de269bdf652b5e075805d969b2
Maharshi08 <maharshi8m86@gmail.com>
2026-04-28 17:51:13 +0530

    fix jenkins dev compose with ci file

---
+++ frontend/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const target = process.env.API_PROXY_TARGET || "http://localhost:5000";
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target,
+      changeOrigin: true,
+    })
+  );
+};
